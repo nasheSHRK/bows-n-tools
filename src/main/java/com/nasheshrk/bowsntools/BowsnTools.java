a7968742fc04d80d876d4631fa0710a5b7113564
NSHRK <96633139+nasheSHRK@users.noreply.github.com>
2021-12-24 16:22:04 -0300

    Mod info

--- src/main/java/com/nasheshrk/bowsntools/BowsnTools.java
+++ src/main/java/com/nasheshrk/bowsntools/BowsnTools.java
@@ -21,7 +21,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
+// Mod Info
 @Mod(BowsnTools.MOD_ID)
 public class BowsnTools
 {
